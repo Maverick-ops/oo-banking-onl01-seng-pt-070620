33c1a6562836c512571fdb028969c80e8a4fb42b
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-10 21:11:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,7 @@ class Transfer
       sender.deposit (amount * -1)
       receiver.deposit (amount)
       @status == "complete"
-    else if 
-      @status == "complete"
-      ""
+    end 
+      
     end 
 end
