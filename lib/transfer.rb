53d150dbd6a647c7377387a1ebe40ba14a2310f0
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-10 21:13:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       "Transaction was already executed"
     else
       @status = "rejected"
-      "Transaction was rejected. Please check your account"
+      "Transaction was rejected. Please check your account."
       end 
       
     end 
